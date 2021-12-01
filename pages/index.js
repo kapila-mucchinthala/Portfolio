1dfbe291b608ba1b8e7f9669c7ef76de1ea0e01d
kapila-mucchinthala <kapilamucchinthala@gmail.com>
2021-12-01 12:15:58 +0530

    Portfolio

---
+++ pages/index.js
@@ -0,0 +1,202 @@
+import dynamic from "next/dynamic";
+import PropTypes from "prop-types";
+import { useRef } from "react";
+import { openSource } from "../portfolio";
+import SEO from "../components/SEO";
+import React, { useState, useEffect } from "react";
+import { greetings } from "../portfolio";
+import Headroom from "headroom.js";
+import {
+  UncontrolledCollapse,
+  NavbarBrand,
+  Navbar,
+  NavItem,
+  Nav,
+  Container,
+  Row,
+  Col,
+  Button,
+} from "reactstrap";
+
+const Greetings = dynamic(() => import("../containers/Greetings"));
+const Skills = dynamic(() => import("../containers/Skills"));
+const Proficiency = dynamic(() => import("../containers/Proficiency"));
+const Education = dynamic(() => import("../containers/Education"));
+const Projects = dynamic(() => import("../containers/Projects"));
+const GithubProfileCard = dynamic(() =>
+  import("../components/GithubProfileCard")
+);
+
+const scrollToRef = (ref) => window.scrollTo(0, ref.current.offsetTop);
+
+export default function Home({ githubProfileData }) {
+  const [collapseClasses, setCollapseClasses] = useState("");
+  const onExiting = () => setCollapseClasses("collapsing-out");
+
+  const onExited = () => setCollapseClasses("");
+
+  useEffect(() => {
+    let headroom = new Headroom(document.getElementById("navbar-main"));
+    headroom.init();
+  });
+  const home = useRef(null);
+  const skills = useRef(null);
+  const education = useRef(null);
+  const project = useRef(null);
+  const contact = useRef(null);
+
+  const homeScroll = () => {
+    scrollToRef(home);
+  };
+
+  const skillScroll = () => {
+    scrollToRef(skills);
+  };
+
+  const educationScroll = () => {
+    scrollToRef(education);
+  };
+
+  const projectScroll = () => {
+    scrollToRef(project);
+  };
+
+  const contactScroll = () => {
+    scrollToRef(contact);
+  };
+
+  return (
+    <div>
+      <SEO
+        data={{
+          title: "Kapila Mucchinthala",
+          description:
+            "A passionate Full Stack Web Developer and Mobile App Developer.",
+          image: "https://avatars3.githubusercontent.com/u/59178380?v=4",
+          url: "https://developer-portfolio-1hanzla100.vercel.app",
+          keywords: [
+            "Kapila",
+            "Kapila Mucchinthala",
+            "@kapilamucchinthala",
+            "kapilamucchinthala",
+            "Portfolio",
+            "Kapila Mucchinthala Portfolio ",
+            "Kapila Mucchinthala Portfolio",
+            "web developer",
+            "full stack",
+            "full stack web developer",
+            "mobile app developer",
+            "android developer",
+            "django",
+            "flask",
+            "django rest framework",
+            "nodejs ",
+            "expressjs",
+            "reactjs ",
+            "contextapi",
+            "redux",
+            "flutter",
+          ],
+        }}
+      />
+
+      <header className="header-global">
+        <div ref={home}>
+          <Navbar
+            ref={home}
+            className="navbar-main navbar-transparent navbar-light headroom"
+            expand="lg"
+            id="navbar-main"
+          >
+            <Container>
+              <NavbarBrand href="/" className="mr-lg-5">
+                <h2 className="text-white" id="nav-title">
+                  {greetings.name}
+                </h2>
+              </NavbarBrand>
+              <button
+                className="navbar-toggler"
+                aria-label="navbar_toggle"
+                id="navbar_global"
+              >
+                <span className="navbar-toggler-icon" />
+              </button>
+              <UncontrolledCollapse
+                toggler="#navbar_global"
+                navbar
+                className={collapseClasses}
+                onExiting={onExiting}
+                onExited={onExited}
+              >
+                <div className="navbar-collapse-header">
+                  <Row>
+                    <Col className="collapse-brand" xs="6">
+                      <h3 className="text-black" id="nav-title">
+                        {greetings.name}
+                      </h3>
+                    </Col>
+                    <Col className="collapse-close" xs="6">
+                      <button className="navbar-toggler" id="navbar_global">
+                        <span />
+                        <span />
+                      </button>
+                    </Col>
+                  </Row>
+                </div>
+                <Nav className="align-items-lg-center ml-lg-auto" navbar>
+                  <NavItem>
+                    <Button onClick={homeScroll}>Home</Button>
+                  </NavItem>
+                  <NavItem>
+                    <Button onClick={skillScroll}>Skills</Button>
+                  </NavItem>
+                  <NavItem>
+                    <Button onClick={educationScroll}>Education</Button>
+                  </NavItem>
+                  <NavItem>
+                    <Button onClick={projectScroll}>Projects</Button>
+                  </NavItem>
+                  <NavItem>
+                    <Button onClick={contactScroll}>Contact</Button>
+                  </NavItem>
+                </Nav>
+              </UncontrolledCollapse>
+            </Container>
+          </Navbar>
+        </div>
+      </header>
+      <div>
+       <Greetings prof={githubProfileData} />
+      </div>
+      <div ref={skills}>
+        <Skills />
+      </div>
+      <div ref={education}>
+        <Proficiency />
+      </div>
+      <div ref={education}>
+        <Education />
+      </div>
+      <div ref={project}>
+        <Projects />
+      </div>
+      <div ref={contact}>
+        <GithubProfileCard prof={githubProfileData} />
+      </div>
+    </div>
+  );
+}
+
+Home.prototype = {
+  githubProfileData: PropTypes.object.isRequired,
+};
+
+export async function getStaticProps(_) {
+  const githubProfileData = await fetch(
+    `https://api.github.com/users/${openSource.githubUserName}`
+  ).then((res) => res.json());
+
+  return {
+    props: { githubProfileData },
+  };
+}
---
+++ portfolio.js
@@ -0,0 +1,175 @@
+import emoji from "react-easy-emoji";
+
+export const greetings = {
+	name: "Kapila Mucchinthala",
+	title: "Hi all, I'm Kapila Mucchinthala",
+	description:
+		"A passionate Full Stack Web Developer having an experience of building Web applications with JavaScript / Reactjs / Nodejs /  and some other cool libraries and frameworks .",
+	resumeLink: "https://drive.google.com/file/d/1Wgb0CyUSpOTa4Yi1o4JJHZvQgsq6lXKT/view?usp=sharing",
+};
+
+export const openSource = {
+	githubUserName: "kapila-mucchinthala",
+};
+
+export const contact = {};
+
+export const socialLinks = {
+	facebook: "https://www.facebook.com/kapila.rao.56/ ",
+	instagram: "https://www.instagram.com/kapilamucchinthala",
+	twitter: "https://twitter.com/KMuchintala",
+	github: "https://github.com/kapila-mucchinthala",
+	linkedin: "https://www.linkedin.com/in/kapila-mucchinthala-498424216/",
+};
+
+export const skillsSection = {
+	title: "What I do",
+	subTitle:
+		"CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+	skills: [
+		emoji(
+			"⚡ Develop highly interactive Front end / User Interfaces for your web applications"
+		),
+		emoji(
+			"⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"
+		),
+		
+	],
+
+	softwareSkills: [
+		{
+			skillName: "html-5",
+			fontAwesomeClassname: "vscode-icons:file-type-html",
+		},
+		{
+			skillName: "css3",
+			fontAwesomeClassname: "vscode-icons:file-type-css",
+		},
+		{
+			skillName: "sass",
+			fontAwesomeClassname: "logos:sass",
+		},
+		{
+			skillName: "JavaScript",
+			fontAwesomeClassname: "logos:javascript",
+		},
+		
+		{
+			skillName: "reactjs",
+			fontAwesomeClassname: "vscode-icons:file-type-reactjs",
+		},
+		{
+			skillName: "nodejs",
+			fontAwesomeClassname: "logos:nodejs-icon",
+		},
+		
+		{
+			skillName: "npm",
+			fontAwesomeClassname: "vscode-icons:file-type-npm",
+		},
+		{
+			skillName: "mongoDB",
+			fontAwesomeClassname: "vscode-icons:file-type-mongo",
+		},
+		
+		{
+			skillName: "git",
+			fontAwesomeClassname: "logos:git-icon",
+		},
+	],
+};
+
+export const SkillBars = [
+	{
+		Stack: "Frontend/Design", //Insert stack or technology you have experience in
+		progressPercentage: "90", //Insert relative proficiency in percentage
+	},
+	{
+		Stack: "Backend",
+		progressPercentage: "70",
+	},
+	{
+		Stack: "Programming",
+		progressPercentage: "70",
+	},
+];
+
+export const educationInfo = [
+
+	{
+		schoolName: "Masai School",
+		subHeader: "Full Stack Web Development",
+		duration: "2021 - 2022",	
+	},
+	{
+		schoolName: "SVS College Of Engineering",
+		subHeader: "Computer Science and Engineering",
+		duration: "2013 - 2017",
+	},
+	// 	{
+	// 	schoolName: "SR Junior College",
+	// 	subHeader: "Intermediate",
+	// 	duration: "2011 - 2013",
+		
+	// },
+];
+
+export const experience = [
+	{
+		role: "Software Engineer",
+		company: "Google",
+		companylogo: "/img/icons/common/google.svg",
+		date: "June 2018 – Present",
+		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		descBullets: [
+			"Lorem ipsum dolor sit amet, consdfgectetur adipiscing elit",
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+		],
+	},
+	{
+		role: "Front-End Developer",
+		company: "Github",
+		companylogo: "/img/icons/common/github.svg",
+		date: "May 2017 – May 2018",
+		desc: "Lorem ipsum dolor sit amet, consefdctetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+	},
+	{
+		role: "Software Engineer Intern",
+		company: "Airbnb",
+		companylogo: "/img/icons/common/airbnbLogo.png",
+		date: "Jan 2015 – Sep 2015",
+		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+	},
+];
+
+export const projects = [
+	{
+		name: "lorem ipsum",
+		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+		link: "https://example.com",
+	},
+	{
+		name: "lorem ipsum",
+		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+		github: "https://github.com/1hanzla100",
+	},
+	{
+		name: "lorem ipsum",
+		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+		github: "https://github.com/1hanzla100",
+		link: "https://example.com",
+	},
+];
+
+export const feedbacks = [
+	{
+		name: "Hassan Mehmood",
+		feedback:
+			"We have been very happy with our new website! It looks professional and very easy to navigate. Our experience with the customer service at Hanzla tauqeer has been great. They handle things very efficiently and are available for any questions we have. They also keep us updated on daily reports so we know how our site is doing. I would recommend that u have choose hanzla web developer services for u.",
+	},
+	{
+		name: "Maheen Altaf",
+		feedback:
+			"the website is very nice, everything was perfectly made as i need. it just loads in moments, without giving u the chance to wait. the quality is also very amazing. i am using it without any problem. great job",
+	},
+];
